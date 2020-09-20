de497cd2571029019b9f020fcdd42cae6eff9a0d
Brian I <brianingram78@gmail.com>
2020-09-20 13:09:37 -0600

    completed server.js file

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var PORT = process.env.PORT || 3000;
+var app = express();
+
+//allow access to public folder
+app.use(express.static("public"));
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+app.use(routes);
+
+//start listener
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
